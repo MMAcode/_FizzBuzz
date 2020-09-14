a94cb19f4e6675721413dcec46dff6674f6c542f
MMAcode <miroslav.makarov@gmail.com>
2020-09-14 11:33:56 +0100

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,35 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.print("ahoj");
-        System.out.print("ahoj2");
-        System.out.print("ahoj git acm 'message', then git push ");
+        System.out.println("Fizz Buzz challenge:");
+        //TestClasMiro test1 = new TestClasMiro("ahoj");
+        //System.out.println(test1.getStrPu());
+
+
+
+        String text = "";
+        int nr = 0;
+        for (int i=1; i<101; i++){
+            nr = i % 3;
+
+            if (i%3==0) {
+                text = text + "Fizz";
+            }
+            if (i%5==0) {
+                text = text + "Buzz";
+            }
+
+
+
+            if (text=="") {
+                System.out.println(i + ")" + i);
+            } else {
+                System.out.println(i + ")" + text);
+            }
+            text = "";
+        }
+
+
 
     }
 }
---
+++ src/com/company/TestClasMiro.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class TestClasMiro {
+    public String strPu = "public strPu with value by default";
+    private String strPr = "public strPr with value by default";
+    private String strCo;
+
+    public TestClasMiro(String content){
+        this.strCo = content;
+    }
+
+    public String getStrPu() {
+        return strPu;
+    }
+
+    public void setStrPu(String strPu) {
+        this.strPu = strPu;
+    }
+
+    public String getStrPr() {
+        return strPr;
+    }
+
+    public void setStrPr(String strPr) {
+        this.strPr = strPr;
+    }
+
+    public String getStrCo() {
+        return strCo;
+    }
+
+    public void setStrCo(String strCo) {
+        this.strCo = strCo;
+    }
+}
