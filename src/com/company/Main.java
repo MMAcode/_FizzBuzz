11c1b091e13da70792cf0174bbe545690a224071
MMAcode <miroslav.makarov@gmail.com>
2020-09-14 17:32:04 +0100

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,57 +4,33 @@ import java.util.Collections;
 
 public class Main {
 
-
-    //text-modifying f. without 'rewrite' boolean attribute
-    public static ArrayList<String> addTextConditionaly(ArrayList<String> originalString, String textToAdd, int currentNumber, int multiplier) {
-        boolean rewrite = false;
-        if (currentNumber % multiplier == 0) {
-            originalString.add(textToAdd);
-        }
-        return originalString;
-    }
-
-    //text-modifying f. with 'rewrite' boolean attribute
-    public static ArrayList<String> addTextConditionaly(ArrayList<String> originalString, String textToAdd, int currentNumber, int multiplier, boolean rewrite, String considerMultipleOf13) {
-        if (currentNumber % multiplier == 0) {
-            originalString.add(textToAdd);
-
-            //Rewrite the text totally if requested.
-            if (rewrite){
-                originalString = new ArrayList<>();
-
-
-                //Watch out for multiple of 13.
-                if (!considerMultipleOf13.equals("")){
-                    originalString.add(considerMultipleOf13);
-                    originalString.add(textToAdd);
-                }else {
-                    originalString.add(textToAdd);
-                }
-            }
-        }
-        return originalString;
-    }
-
     public static void main(String[] args) {
-        //testing new branch
-        //ignore this
 
         System.out.println("Fizz Buzz challenge:");
         ArrayList<String> texts = new ArrayList<>();
 
         //iterate through all numbers
         for (int i = 1; i < 301; i++) {
+
             //Prepare text filed to display.
-            texts = addTextConditionaly(texts, "Fizz", i, 3);
-            texts = addTextConditionaly(texts, "Fezz", i, 13);
-            texts = addTextConditionaly(texts, "Buzz", i, 5);
-            texts = addTextConditionaly(texts, "Bang", i, 7);
+            if(i % 3 == 0) texts.add("Fizz");
+            if(i % 13 == 0) texts.add("Fezz");
+            if(i % 5 == 0) texts.add("Buzz");
+            if(i % 7 == 0) texts.add("Bang");
 
-            // handle multiple of 11 but watch-ut for multiple of 13 too (last parameter
-            texts = addTextConditionaly(texts, "Bong", i, 11, true, i%13==0 ? "Fezz" : "");
+            if(i % 11 == 0) {
+                //quick solution:
+                //                ArrayList<String> texts = new ArrayList<>();
+                //                if(i % 13 == 0) texts.add("Fezz");
+                //                texts.add("Bong");
 
-            //swap words if multiple of 17
+                //solution without using %13:
+                ArrayList<String> textsSupportVar = new ArrayList<>();
+                if(texts.indexOf("fezz") >= 0 ) textsSupportVar.add("Fezz");
+                textsSupportVar.add("Bong");
+                texts = textsSupportVar;
+            }
+
             if (i % 17 == 0){
                 System.out.print("This is----------------- 17TT: ");
                 Collections.reverse(texts);
