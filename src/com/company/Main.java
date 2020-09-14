f9971ccd118d9f8f84276f57602afbe679512f6f
MMAcode <miroslav.makarov@gmail.com>
2020-09-14 15:58:16 +0100

    new branch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,28 +1,35 @@
 package com.company;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
 
+
     //text-modifying f. without 'rewrite' boolean attribute
-    public static String addTextConditionally(String originalString, String textToAdd, int currentNumber, int multiplier) {
+    public static ArrayList<String> addTextConditionally(ArrayList<String> originalString, String textToAdd, int currentNumber, int multiplier) {
         boolean rewrite = false;
         if (currentNumber % multiplier == 0) {
-            originalString += textToAdd;
+            originalString.add(textToAdd);
         }
         return originalString;
     }
 
     //text-modifying f. with 'rewrite' boolean attribute
-    public static String addTextConditionally(String originalString, String textToAdd, int currentNumber, int multiplier, boolean rewrite, String considerMultipleOf13) {
+    public static ArrayList<String> addTextConditionally(ArrayList<String> originalString, String textToAdd, int currentNumber, int multiplier, boolean rewrite, String considerMultipleOf13) {
         if (currentNumber % multiplier == 0) {
-            originalString += textToAdd;
+            originalString.add(textToAdd);
 
             //Rewrite the text totally if requested.
             if (rewrite){
-                originalString = textToAdd;
+                originalString = new ArrayList<>();
+
 
                 //Watch out for multiple of 13.
-                if(!considerMultipleOf13.equals("")){
-                    originalString = considerMultipleOf13 + textToAdd;
+                if (!considerMultipleOf13.equals("")){
+                    originalString.add(considerMultipleOf13);
+                    originalString.add(textToAdd);
+                }else {
+                    originalString.add(textToAdd);
                 }
             }
         }
@@ -31,26 +38,34 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Fizz Buzz challenge:");
-
-
-        String text = "";
+        ArrayList<String> texts = new ArrayList<>();
 
         //iterate through all numbers
-        for (int i = 1; i < 201; i++) {
+        for (int i = 1; i < 301; i++) {
             //Prepare text filed to display.
-            text = addTextConditionally(text, "Fizz", i, 3);
-            text = addTextConditionally(text, "Fezz", i, 13);
-            text = addTextConditionally(text, "Buzz", i, 5);
-            text = addTextConditionally(text, "Bang", i, 7);
+            texts = addTextConditionally(texts, "Fizz", i, 3);
+            texts = addTextConditionally(texts, "Fezz", i, 13);
+            texts = addTextConditionally(texts, "Buzz", i, 5);
+            texts = addTextConditionally(texts, "Bang", i, 7);
 
             // handle multiple of 11 but watch-ut for multiple of 13 too (last parameter
-            text = addTextConditionally(text, "Bong", i, 11, true, i%13==0 ? "Fezz" : "");
+            texts = addTextConditionally(texts, "Bong", i, 11, true, i%13==0 ? "Fezz" : "");
+
+            //swap words if multiple of 17
+            if (i % 17 == 0){
+//                ArrayList<String> texts2 = new ArrayList<>();
+                System.out.print("This is----------------- 17TT: ");
+                Collections.reverse(texts);
+                //unfinished
+            }
 
             //Display the text field conditionally.
-            System.out.println(i + ")" + (text.equals("") ? i : text) );
+            System.out.println(i + ")" + (texts.size()==0 ? i : (texts.stream().reduce("",(accumulator,currentValue)->accumulator+currentValue)) ) );
 
-            //Reset text field.
-            text = "";
-            }
+            //Reset texts
+            texts = new ArrayList<>();
+        }
     }
+
+
 }
