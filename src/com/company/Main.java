90dcea4fd2c786127871c79298af633ce52762e2
MMAcode <miroslav.makarov@gmail.com>
2020-09-15 12:25:58 +0100

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,4 +1,5 @@
 package com.company;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
@@ -7,13 +8,12 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //user input 1: Get number of numbers to display
         System.out.println("Fizz Buzz challenge:");
         ArrayList<String> texts = new ArrayList<>();
-
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("How many numbers would you like to see?: ");
-        while(!scanner.hasNextInt()){
+        while (!scanner.hasNextInt()) {
             scanner.nextLine();
             System.out.println("try again");
             System.out.print("How many numbers would you like to see?: ");
@@ -21,34 +21,42 @@ public class Main {
         int amountOfNumbers = scanner.nextInt();
         scanner.nextLine();
 
+        //user input 2: Get rules/multiples
         System.out.println();
         System.out.println("Which rules/multiples would you like to implement?");
         System.out.print("Type numbers separated by spaces (e.g.: '3 7 5 11'): ");
-//        while(!scanner.hasNextInt()){
-//            scanner.nextLine();
-//            System.out.println("try again");
-//            System.out.print("How many numbers would you like to see?: ");
-//        }
         String requestedMultiples = scanner.nextLine();
-        System.out.println("received: " + requestedMultiples + " end" + requestedMultiples.length()) ;
+        //System.out.println("received: " + requestedMultiples + " end" + requestedMultiples.length());
 
-//        get multiples from the string
+        // get multiples from the string
+        ArrayList<String> rules = new ArrayList<>();
+        for (int i = 0; i < requestedMultiples.length(); i++) {
+            char currentCharacter = requestedMultiples.charAt(i);
 
-
-
-
+            //check if there is more digits for this number
+            String supportStringForMultiDigit = "";
+            do {
+                supportStringForMultiDigit +=currentCharacter;
+                i++;
+                if(i < requestedMultiples.length()) currentCharacter = requestedMultiples.charAt(i);
+            }
+            while(i < requestedMultiples.length() && Character.isDigit(currentCharacter));
+            rules.add(supportStringForMultiDigit);
+        }
+        //show rules
+        System.out.println("Rules/multiples received: "+ rules);
 
 
         //iterate through all numbers
         for (int i = 1; i <= amountOfNumbers; i++) {
 
             //Prepare text filed to display.
-            if(i % 3 == 0) texts.add("Fizz");
-            if(i % 13 == 0) texts.add("Fezz");
-            if(i % 5 == 0) texts.add("Buzz");
-            if(i % 7 == 0) texts.add("Bang");
+            if (i % 3 == 0) texts.add("Fizz");
+            if (i % 13 == 0) texts.add("Fezz");
+            if (i % 5 == 0) texts.add("Buzz");
+            if (i % 7 == 0) texts.add("Bang");
 
-            if(i % 11 == 0) {
+            if (i % 11 == 0) {
                 //quick solution:
                 //                ArrayList<String> texts = new ArrayList<>();
                 //                if(i % 13 == 0) texts.add("Fezz");
@@ -56,18 +64,18 @@ public class Main {
 
                 //solution without using %13:
                 ArrayList<String> textsSupportVar = new ArrayList<>();
-                if(texts.indexOf("fezz") >= 0 ) textsSupportVar.add("Fezz");
+                if (texts.indexOf("fezz") >= 0) textsSupportVar.add("Fezz");
                 textsSupportVar.add("Bong");
                 texts = textsSupportVar;
             }
 
-            if (i % 17 == 0){
+            if (i % 17 == 0) {
                 System.out.print("This is----------------- 17TT: ");
                 Collections.reverse(texts);
             }
 
             //Display the text field conditionally.
-            System.out.println(i + ")" + (texts.size()==0 ? i : (texts.stream().reduce("",(accumulator,currentValue)->accumulator+currentValue)) ) );
+            System.out.println(i + ")" + (texts.size() == 0 ? i : (texts.stream().reduce("", (accumulator, currentValue) -> accumulator + currentValue))));
 
             //Reset texts
             texts = new ArrayList<>();
